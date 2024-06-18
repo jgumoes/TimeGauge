7752bcbb9bb9d247d422a4e062636483091e158f
Jack Gumoes <j.gumoes@gmail.com>
2024-06-18 17:34:46 +0100

    restructured project for VS Code - eslint configured - typescript not used as it doesn't play well with espruino's globally availlable objects - gulpfile is unused as it doesn't do anything useful yet - TODO: edit gulpfile to bundle multiple source files into one

--- src/timegauge.app.js
+++ src/timegauge.app.js
@@ -4,13 +4,16 @@
 require("Font7x11Numeric7Seg").add(Graphics);
 // position on screen
 const X = g.getWidth()/2, Y = g.getHeight()/2;
-var isScreenOn = true;
+const isScreenOn = true;
+// const X = g.getWidth()/2, Y = g.getHeight()/2;
 
 const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 function clearScreen(){
 	g.clear();
+	// @ts-expect-error: Cannot find name error
 	Bangle.loadWidgets();
+	// @ts-expect-error: Cannot find name error
 	Bangle.drawWidgets();
 }
 
@@ -33,7 +36,7 @@ function diffToString(timeDiff){
 // functions for the time list
 let scroller;
 const minTime = 5000;
-var times = {
+const times = {
 	maxLength: 10,
 	list: [],
 	addTime: function(newMSTime){
@@ -59,10 +62,10 @@ var times = {
 	clearMarked: function(){this.list.forEach(t => t.d=false);},
 	anyMarked: function(){return this.list.some(t => t.d);},
 	makeStrObj: function(i){
-		var iTime = new Date(this.list[i].t);
-		var ts = padTime(iTime.getHours())+":"+padTime(iTime.getMinutes())+":"+padTime(iTime.getSeconds());
-		var ds = "";
-		var prevTime;
+		const iTime = new Date(this.list[i].t);
+		const ts = padTime(iTime.getHours())+":"+padTime(iTime.getMinutes())+":"+padTime(iTime.getSeconds());
+		let ds = "";
+		let prevTime;
 		if(i > 0){
 			//ds = diffToString(this.list[i-1].t - this.list[i].t);
 			prevTime = this.list[i-1].t;
@@ -80,11 +83,11 @@ var times = {
 
 function devTimes(length){
 	console.log("DELETE devTimes");
-	var diffArr = new Array(length);
+	const diffArr = new Array(length);
 	//console.log(`is DST? ${(new Date()).getIsDST()}`);
-	var milliseconds = Math.round(Date.now());
-	for(i = 0; i < length-1; i++){
-		let milli = Math.round(Math.random()*10*60 + 5*60)*1000;
+	let milliseconds = Math.round(Date.now());
+	for(let i = 0; i < length-1; i++){
+		const milli = Math.round(Math.random()*10*60 + 5*60)*1000;
 		milliseconds -= milli;
 		diffArr[i] = milli;
 	}
@@ -96,38 +99,38 @@ function devTimes(length){
 }
 
 function drawScroller(){
-	var timeStrings = times.makeStrings();
+	const timeStrings = times.makeStrings();
 	//console.log("timeStrings: ", timeStrings);
 	//console.log(`is DST? ${(new Date()).getIsDST()}`);
 	return E.showScroller({
-  	h : 50, // height of each menu item in pixels
-  	c : timeStrings.length, // number of menu items
-  	// a function to draw a menu item
-  	draw : (idx, r) => {
+		h : 50, // height of each menu item in pixels
+		c : timeStrings.length, // number of menu items
+		// a function to draw a menu item
+		draw : (idx, r) => {
 			//console.log("drawing: ", idx);
-			var numObj = timeStrings[idx];
+			const numObj = timeStrings[idx];
 			//console.log(`${i} marked for delete? ${times.list[idx]}`);
 			g.setBgColor((!times.list[idx].d)?"#666":"#900").clearRect(r.x,r.y,r.x+r.w-1,r.y+r.h-1);
 			// draw dividers
 			//g.drawLine(r.x,r.y,r.x+r.w-1,r.y);g.drawLine(r.x,r.y+r.h-1,r.x+r.w-1,r.y+r.h-1);
 			g.setFont("6x8:2").setFontAlign(1, -1, 0).drawString(numObj.d,r.x+r.w-1,r.y+4);
 			g.setFont("6x8:2").setFontAlign(-1, 1, 0).drawString(numObj.t,r.x+10,r.y+r.h-4);
-	  },
-	  select : function(idx, touch) {
+		},
+		select : function(idx, touch) {
 			//console.log(`selected ${i}: {t:${timeStrings[i].t},d:${timeStrings[i].d}}`);
 			times.markForDel(idx);
 		},
 		// optional function to be called when 'back' is tapped
-	  back : function() {
+		back : function() {
 			console.log("back pressed (scroller)");
 			if(times.anyMarked()){
 				console.log("times were marked for delete");
 				E.showPrompt("Delete times?").then(function(v) {
-	  			if (v){
+					if (v){
 						console.log("'Yes' chosen");
 						times.deleteMarked();
 					}
-	  			else {
+					else {
 						console.log("'No' chosen");
 						times.clearMarked();
 					}
@@ -146,7 +149,7 @@ function drawScroller(){
 
 function drawDate(dateObj, dateFontsize, dateY, alignY){
 	//display the current date
-	var dateString = daysOfTheWeek[dateObj.getDay()] + "\n" + dateObj.getDate().toString() + "/" + (dateObj.getMonth()+1) + "/" + dateObj.getFullYear();
+	const dateString = daysOfTheWeek[dateObj.getDay()] + "\n" + dateObj.getDate().toString() + "/" + (dateObj.getMonth()+1) + "/" + dateObj.getFullYear();
 	//g.setFont("7x11Numeric7Seg",dateFontsize);
 	g.setFont("Vector", dateFontsize);
 	g.setFontAlign(0, alignY);
@@ -159,17 +162,17 @@ function drawDiff(){
 	g.drawRect(2, yMax-50, g.getWidth()-3, yMax-2);
 	g.setFont("Vector", 20);
 	g.setFontAlign(0,0);
-	var diffStr = times.makeStrObj(0).d||"0s";
+	const diffStr = times.makeStrObj(0).d||"0s";
 	g.drawString(diffStr,X,yMax-25, true);
 }
 
 function updateClock() {
-	var d = new Date();
+	const d = new Date();
 	g.reset();
 	g.drawLine(0, 24, g.getWidth(), 24);
 	//display the current time
-  var h = d.getHours(), m = d.getMinutes();
-	var time = padTime(h)+":"+padTime(m)+ ":"+padTime(d.getSeconds());
+  const h = d.getHours(), m = d.getMinutes();
+	const time = padTime(h)+":"+padTime(m)+ ":"+padTime(d.getSeconds());
   g.setFont("7x11Numeric7Seg",3);
   g.setFontAlign(0,0);
   g.drawString(time, X, Y, true /*clear background*/);
@@ -190,8 +193,8 @@ clearScreen();
 
 // initiate timers, intervals, etc.
 // return a callable function to clear the timers
-var updateInterval = null;
-var scrollerTimeout = null;
+let updateInterval = null;
+const scrollerTimeout = null;
 const initView={
 	"updatingClock": ()=>{
 		updateClock();
@@ -245,6 +248,7 @@ const swipeHandlers={
 	"updatingClock": function(dLR, dUD){
 		if(dUD === 1){
 			// TODO: this needs to be cancelled when timesList opens
+			// @ts-expect-error: Cannot find name error
 			Bangle.setLocked(true);
 		} else if(dUD === -1){
 			//console.log("todo: show previous times!");
@@ -256,6 +260,7 @@ const swipeHandlers={
 };
 
 // event handlers
+// @ts-expect-error: Cannot find name error
 Bangle.on('swipe', function(directionLR, directionUD) {
 	//console.log(`swiped @ ${(new Date()).getTime()}: ${directionLR}, ${directionUD}`);
 	swipeHandlers[vc.appView](directionLR, directionUD);
@@ -271,7 +276,7 @@ const lockHandlers={
 	},
 	"timeList": function(off, reason){console.log("todo: timeList lockHandlers");}
 };
-
+// @ts-expect-error: Cannot find name error
 Bangle.on('lock', function(off, reason) {
 	console.log("Bangle screen locked: ", off);
 	console.log("lock reason: ", reason);
@@ -279,6 +284,7 @@ Bangle.on('lock', function(off, reason) {
 });
 
 devTimes(10);
+// @ts-expect-error: Cannot find name error
 if(Bangle.isLocked()){
 	vc.switchView("frozenClock");
 }
